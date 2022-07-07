b7247824480245fc1e17fd5c21a9d6905997fb1f
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-07-07 08:50:35 -0300

    button done

---
+++ components/Temporizador.tsx
@@ -0,0 +1,24 @@
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+import styles from '../styles/Temporizador.module.css'
+
+interface TemporizadorProps{
+    duracao: number
+    tempoEsgotado: () => void
+}
+
+export default function Temporizador(props: TemporizadorProps){
+    return(
+        <div className={styles.temporizador}>
+            <CountdownCircleTimer 
+                duration={props.duracao}
+                size={120}
+                isPlaying
+                onComplete={props.tempoEsgotado}
+                colors={['#BCE596','#F7B801','#ED827A']}//so pega a primeira cor
+                colorsTime={[props.duracao*0.33, props.duracao*0.33, props.duracao*0.33]}
+            >
+                {({remainingTime}) => remainingTime}
+            </CountdownCircleTimer>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Botao from '../components/Botao'
 import Questao from '../components/Questao'
 import QuestaoModel from '../model/questao'
 import RespostaModel from '../model/resposta'
@@ -12,13 +13,23 @@ const questaoMock = new QuestaoModel(1,'Melhor cor',[
 
 export default function Home() {
   const [questao, setQuestao] = useState(questaoMock)
+
   function respostaFornecida(indice: number) {
     setQuestao(questao.responderCom(indice))
   }
+
+  function tempoEsgotado() {
+    if(questao.naoRespondida) setQuestao(questao.responderCom(-1))
+  }
   
   return (
-    <div style={{display: 'flex',height:'100vh',justifyContent:'center',alignItems:'center'}}>
-      <Questao valor={questao} respostaFornecida={respostaFornecida} />
+    <div style={{display: 'flex',height:'100vh',justifyContent:'center',alignItems:'center', flexDirection:'column'}}>
+      <Questao 
+      valor={questao} 
+      tempoParaResposta={5}
+      respostaFornecida={respostaFornecida}
+      tempoEsgotado={tempoEsgotado} />
+      <Botao texto="Próxima" href='/resultado' />
     </div>
   )
 }
